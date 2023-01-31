407fee7efb4fb1d9cc04ba4356898539d8c92d1a
Cole Yeager <106110474+ColeYeager19@users.noreply.github.com>
2023-01-30 20:18:42 -0700

    Create JS.js

---
+++ JS.js
@@ -0,0 +1,33 @@
+// JavaScript Document
+
+/*This console log does not function when using dreamweaver
+though it does operate fully once thrown into a different
+coding program*/
+
+var name = "Cole Yeager";
+
+var website1 = "openpuppies.com";
+var website2 = "youtube.com";
+var website3 = "khanacademy.org";
+
+var game1 = "Assassins Creed Valhalla";
+var game2 = "Minecraft";
+var game3 = "Rainbow Six Siege";
+
+var artist1 = "Richard Serra, one of the few artists capable of creating something so offputing that the instillation would be removed";
+var artist2 = "Robert Frost, a multiple time pulitzer winner and a personal inspiration";
+var artist3 = "Casper David Friedrich, one of the greatest romantic landscape painters Germany had to offer";
+
+console.log ("name:", name)
+
+console.log ("website1", website1)
+console.log ("website2", website2)
+console.log ("website3", website3)
+
+console.log ("game1", game1)
+console.log ("game2", game2)
+console.log ("game3", game3)
+
+console.log ("artist1", artist1)
+console.log ("artist2", artist2)
+console.log ("artist3", artist3)
